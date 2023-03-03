aa034197f335b23ec3048c51e58e3eda6e749348
salman-rafii <salmanrafi125@gmail.com>
2023-03-04 02:25:56 +0500

    home screen work in progress

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -5,6 +5,8 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      body: Center(child: Text("Home Screen")),
+    );
   }
 }
