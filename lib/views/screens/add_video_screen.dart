7da2408e878c5803d420f0c04639b1861257696f
salman-rafii <salmanrafi125@gmail.com>
2023-03-08 00:50:50 +0500

    add video is done

--- lib/views/screens/add_video_screen.dart
+++ lib/views/screens/add_video_screen.dart
@@ -1,16 +1,83 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:tiktok_clone_flutter/utils/constants.dart';
+import 'package:tiktok_clone_flutter/views/screens/confirm_video_screen.dart';
 import 'package:tiktok_clone_flutter/views/widgets/custom_text.dart';
 
 class AddVideoScreen extends StatelessWidget {
   const AddVideoScreen({super.key});
+
+  pickVideo(ImageSource src, BuildContext context) async {
+    final video = await ImagePicker().pickVideo(source: src);
+    if (video != null) {
+      var route =
+          MaterialPageRoute(builder: (context) => const ConfirmVideoScreen());
+      Navigator.of(context).push(route);
+    }
+  }
+
+  showOptionsDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (context) => SimpleDialog(
+              children: [
+                SimpleDialogOption(
+                  onPressed: () => pickVideo(ImageSource.gallery, context),
+                  child: Row(
+                    children: const [
+                      Icon(Icons.image),
+                      Padding(
+                        padding: EdgeInsets.all(7.0),
+                        child: CustomText(
+                          text: "Gallery",
+                          fontSize: 20.0,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SimpleDialogOption(
+                  onPressed: () => pickVideo(ImageSource.camera, context),
+                  child: Row(
+                    children: const [
+                      Icon(Icons.camera_alt),
+                      Padding(
+                        padding: EdgeInsets.all(7.0),
+                        child: CustomText(
+                          text: "Camera",
+                          fontSize: 20.0,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SimpleDialogOption(
+                  onPressed: () => Navigator.pop(context),
+                  child: Row(
+                    children: const [
+                      Icon(Icons.cancel),
+                      Padding(
+                        padding: EdgeInsets.all(7.0),
+                        child: CustomText(
+                          text: "Cancel",
+                          fontSize: 20.0,
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: InkWell(
-          onTap: () {},
+          onTap: () => showOptionsDialog(context),
           child: Container(
             width: 190,
             height: 50,
---
+++ lib/views/screens/confirm_video_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ConfirmVideoScreen extends StatelessWidget {
+  const ConfirmVideoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
