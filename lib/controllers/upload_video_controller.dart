a738033c1a25b574519b5ba9d279dd0fbb6b7a5c
salman-rafii <salmanrafi125@gmail.com>
2023-03-10 00:07:54 +0500

    working on upload video functionality

---
+++ lib/controllers/upload_video_controller.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+
+class UploadVideoController extends GetxController {
+  //upload video
+
+  uploadVideo(String songName, String caption, String videoPath) async {
+    try {} catch (e) {}
+  }
+}
